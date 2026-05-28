4412d16b5bc666d4eecfeec8a444039293ff8f36
zang2x <86270394+zang2x@users.noreply.github.com>
2026-05-28 16:56:26 +0200

    añadiendo speed insights de vercel

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Privacy from './pages/Privacy';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
   return (
