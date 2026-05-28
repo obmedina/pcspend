e2771e3950d03d3c4bedbce8516c4b1b1c5ff4f8
zang2x <86270394+zang2x@users.noreply.github.com>
2026-05-28 16:14:29 +0200

    analytics de vercel 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Privacy from './pages/Privacy'; // Asegúrate de tener este componente creado
-import { Analytics } from "@vercel/analytics/next"
+import Privacy from './pages/Privacy';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/privacy" element={<Privacy />} />
-      </Routes>
-    </Router>
+    <>
+      <Analytics /> 
+      
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/privacy" element={<Privacy />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
